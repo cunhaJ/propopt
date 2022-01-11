a88258c734328c509a160a243092584a2048ffcd
CunhaJ <forsolar.cunha@gmail.com>
2022-01-11 08:59:56 +0000

    update functions

--- propopt/propagate.py
+++ propopt/propagate.py
@@ -1,6 +1,7 @@
 ####propagate.py   
 def fresnel(z, mask, npixmask, pixsizemask, npixscreen, dxscreen, dyscreen, wavelength):
-    import numpy as np 
+    import numpy as np
+    import scipy.fftpack as sfft  
     
     k = 2* np.pi/wavelength
     
@@ -18,15 +19,17 @@ def fresnel(z, mask, npixmask, pixsizemask, npixscreen, dxscreen, dyscreen, wave
     ys1 = np.linspace(-dyscreen, dyscreen, nps)
     (xs, ys) = np.meshgrid(xs1, ys1)
     
+    #Goodman, exp 4-17
     v1  = np.exp(1.0j*k* (xm*xm + ym*ym)/ (2*z))
     v2  = np.exp(1.0j*k* (xs*xs + ys*ys)/ (2*z)) 
     v3  = np.exp(1.0j*k*z)/ (1.0j*wavelength*z)
-    Ef = v2 * v3 * np.fft.fftshift(np.fft.fft2(v1*mask))
+    Ef = v2 * v3 * sfft.fftshift(sfft.fft2(v1*mask))
     
     return np.abs(Ef)**2
 
 def fraunhofer(z, mask, npixmask, pixsizemask, npixscreen, dxscreen, dyscreen, wavelength):
-    import numpy as np 
+    import numpy as np
+    import scipy.fftpack as sfft    
     
     k = 2* np.pi/wavelength
     #number of pixels 
@@ -39,20 +42,31 @@ def fraunhofer(z, mask, npixmask, pixsizemask, npixscreen, dxscreen, dyscreen, w
     ym1 = np.linspace(-dmask/2, dmask/2, npm)
     (xm, ym) = np.meshgrid(xm1, ym1)
     
-    zdist = z 
-    wavelength = wavelength 
+    xs1 = np.linspace(-dxscreen, dxscreen, nps)
+    ys1 = np.linspace(-dyscreen, dyscreen, nps)
+    (xs, ys) = np.meshgrid(xs1, ys1)
+    
+    
     delta1=pixsizemask
 
-    delta2 = zdist*wavelength /(npm*delta1)
-    num = np.round(dxscreen/delta2)
+    #delta2 = z*wavelength /(npm*delta1)
+    #num = np.round(dxscreen/delta2)
     #print(num)
     
-    import cv2 
-    resized = cv2.resize(mask,(int(num),int(num)), interpolation = cv2.INTER_AREA)
+    #import cv2 
+    #resized = cv2.resize(mask,(int(4*num),int(4*num)), interpolation = cv2.INTER_AREA)
+    resized = mask
+    
+    #Goodman, exp 4-25
+    v2  = np.exp(1.0j*k* (xs*xs + ys*ys)/ (2*z)) 
+    v3  = np.exp(1.0j*k*z)/ (1.0j*wavelength*z)
+    Ef = v2 * v3* sfft.fftshift(sfft.fft2(resized))
+    
+
     #print(resized.shape)
     
-    v = np.exp(1.0j*k*z) / (1.0j*wavelength*z)
-    Ef = v * np.fft.fftshift(np.fft.fft2(resized))
+    #Ef2 = cv2.resize(Ef,(int(num),int(num)), interpolation = cv2.INTER_AREA)
+    
     return np.abs(Ef)**2
     
 def RS_int(zs, mask, npixmask, pixsizemask, npixscreen, dxscreen, dyscreen, wavelength, I0, verbose =False ): 
@@ -87,7 +101,7 @@ def RS_int(zs, mask, npixmask, pixsizemask, npixscreen, dxscreen, dyscreen, wave
     if nps <= 2*npm: 
         print("The number of screen is not large enough, I will resize them for you.")
         nps = npm*4 
-        print("Rescaled the screen pixels to "+str(nps)+"^2 . The computation will now proceed")
+        print("Rescaled the screen pixels to "+str(nps)+" . The computation will now proceed")
     
     #size of mask 
     dmask = pixsizemask * npm
@@ -154,8 +168,9 @@ def RS_int(zs, mask, npixmask, pixsizemask, npixscreen, dxscreen, dyscreen, wave
             prop1= np.exp(-r*1.0j*k)/r2
             prop2 = zs * (1.0j * k  + unit/r)
             propE = E0m * prop1 * prop2
-            rEs[isc,jsc] = double_Integral(-dmask/2, dmask/2, -dmask/2, dmask/2, npm*100,npm*100,np.real(propE))/(2*np.pi)
-            iEs[isc,jsc] = double_Integral(-dmask/2, dmask/2, -dmask/2, dmask/2, npm*100,npm*100,np.imag(propE))/(2*np.pi)
+            #here npm*400, is a guess for the number of points to calc the int
+            rEs[isc,jsc] = double_Integral(-dmask/2, dmask/2, -dmask/2, dmask/2, npm*400,npm*400,np.real(propE))/(2*np.pi)
+            iEs[isc,jsc] = double_Integral(-dmask/2, dmask/2, -dmask/2, dmask/2, npm*400,npm*400,np.imag(propE))/(2*np.pi)
 
     Escreen = rEs + 1.0j*iEs 
     Iscreen = (c_const*n*eps0/2) * np.abs(Escreen)**2
@@ -214,3 +229,137 @@ def rect_fraun(sizex, sizey, xcoord, ycoord, zdist, wavelength):
     
     return intensity 
 
+
+####Experimenting for the XZ Rayleigh-Sommerfeld 
+
+
+#######FUNCTION THAT CALCULATES THE RS INTEGRAL 
+#here is a function that calculates the RS_int of the first kind, taking information about mask, distance to screen, and screen information
+def RS_int_XZ(zs, nzds, mask, npixmask, pixsizemask, npixscreen, dxscreen, dyscreen, wavelength, I0): 
+    """
+    returns Escreen (complex electric field at obs screen), Iscreen (intensity at obs screen), iplot (the actual intensity) 
+    inputs: 
+    zs = distance to screen [m]
+    mask = image of the mask (normalized to [0,1]) - in principle could be grayscale between 0 and 1 
+    npixmask = number of pixels on the side of the mask 
+    pixsizemask = size of pixel of the mask [m]
+    npixscreen = number of pixels on the side of the screen 
+    dxscreen = x side of the screen [m]
+    dyscreen = y side of the screen [m]
+    wavelength = wavelength of the light [m]
+    I0 = intensity of the light at the mask plane [W/m2]
+    """
+    import decimal
+    # set the precision to double that of float64
+    decimal.setcontext(decimal.Context(prec=34))
+
+    #number of pixels 
+    nps = npixscreen 
+    npm = npixmask 
+    
+    if nps <= 2*npm: 
+        print("The number of screen is not large enough, I will resize them for you.")
+        nps = npm*4 
+        print("Rescaled the screen pixels to "+str(nps)+"^2 . The computation will now proceed")
+    
+    #size of mask 
+    dmask = pixsizemask * npm
+    
+    #physical constants 
+    c_const = 3e8 
+    eps0 = 8.854189e-12 
+    n = 1 #refractive index of medium  
+
+    k = 2* np.pi/wavelength
+
+    ## definitions 
+    unit = np.ones((npm,npm), dtype=complex)
+    r = np.zeros((npm,npm)) 
+    r3 = np.zeros((npm,npm))
+    prop1 = np.zeros((npm,npm))
+    prop2 = np.zeros((npm,npm))
+    propE = np.zeros((npm,npm))
+
+    
+    #electric field real and imaginary and total 
+    rEs =np.zeros ((nps,nps))
+    iEs =np.zeros ((nps,nps))
+    Escreen =np.zeros ((nps,nps), dtype =complex)
+
+    #define the zpos of the mask at 0 
+    zm =0 
+
+    #definition of the mask
+    xm1 = np.linspace(-dmask/2, dmask/2, npm)
+    ym1 = np.linspace(-dmask/2, dmask/2, npm)
+    (xm, ym) = np.meshgrid(xm1,ym1)
+    
+    ##Electric field calc from intensity
+    E0 = np.sqrt(2*I0/(c_const*n*eps0))
+    
+    #definition of the electric field at the mask 
+    E0m = E0 * mask
+
+    fig=plt.figure()
+    plt.imshow(E0m, vmin=0, vmax=1, cmap=plt.get_cmap("jet"))
+    plt.title("Efield at mask")
+
+    #intensity at the mask
+    i0m = (c_const*n*eps0/2)*E0m*E0m 
+    I0m = double_Integral(-dmask/2, dmask/2, -dmask/2, dmask/2, npm,npm, i0m)
+
+    print(I0m)
+
+    Utheoreticalmax = I0 * np.pi * dmask**2 
+    print(Utheoreticalmax)
+
+    xs1 = np.linspace(-dxscreen, dxscreen, nps)
+    ys1 = np.linspace(-dyscreen, dyscreen, nps)
+    (xs, ys) = np.meshgrid(xs1,ys1)
+    
+    print(ys[0,:])
+    
+    
+    #array with the distance in z until the distance zs given as argument 
+    zdistarray = np.linspace(0,zs,nzds)
+    
+    inten = np.zeros((len(xs1), len(zdistarray)))
+    
+    print (inten)
+
+    ###### calculate the Rayleigh Sommerfeld integral 
+    ### ATTENTION TO CHECK WITH GOODMAN BOOK IF CORRECTLY IMPLEMENTED 
+    
+    for iz, zsd in enumerate(zdistarray): 
+        ##### calculate the Rayleigh Sommerfeld integral 
+        #From Oshea formulation, eq 2.8
+        for isc in np.arange(0,nps-1):
+            if verbose == True: 
+                print(isc/nps)
+                
+            for jsc in np.arange(0,nps-1): 
+                r = np.sqrt((xs[isc,jsc]-xm)**2 + (ys[isc,jsc]-ym)**2 + (zs-zm)**2)
+                r2 = r*r
+                prop1= np.exp(-r*1.0j*k)/r2
+                prop2 = zs * (1.0j * k  + unit/r)
+                propE = E0m * prop1 * prop2
+                #here npm*400, is a guess for the number of points to calc the int
+                rEs[isc,jsc] = double_Integral(-dmask/2, dmask/2, -dmask/2, dmask/2, npm*400,npm*400,np.real(propE))/(2*np.pi)
+                iEs[isc,jsc] = double_Integral(-dmask/2, dmask/2, -dmask/2, dmask/2, npm*400,npm*400,np.imag(propE))/(2*np.pi)
+
+        Escreen = rEs + 1.0j*iEs 
+        Iscreen = (c_const*n*eps0/2) * np.abs(Escreen)**2
+        iplot = 10*Iscreen**0.3
+        #print(iplot)
+        midpoint = int(nps/2)-1
+        inten[:,iz] = iplot[:,midpoint]
+        #inten[isc,iz] = iplot
+        
+        print(inten[:,iz])
+        
+        iplotmax = np.max(iplot)
+    
+    return Escreen, Iscreen, inten
+
+###TRIAL TO OBTAIN THE XZ PLAN -> in principle only need to change the r for the distance plane.... 
+####TO CHECK 
